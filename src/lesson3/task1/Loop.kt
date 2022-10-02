1ca7edde1d637d07caffe2ccd892b83366e5a70c
Mariaj240 <chernova.mm@edu.spbstu.ru>
2022-10-02 20:24:03 +0300

    Урок 3 задания 1-4, 6-9 выполнены

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -72,7 +72,16 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun digitNumber(n: Int): Int = TODO()
+fun digitNumber(n: Int): Int {
+    var k = 0
+    var number = n
+    if (n == 0) return 1 else
+        while (number>=1) {
+            k+=1
+            number/=10
+        }
+   return k
+}
 
 /**
  * Простая (2 балла)
@@ -80,21 +89,48 @@ fun digitNumber(n: Int): Int = TODO()
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int = TODO()
+fun fib(n: Int): Int {
+    var fib1 = 1
+    var fib2 = 1
+    var sum = 1
+    for (i in 3.. n) {
+       sum = fib1 + fib2
+       fib2 = fib1
+       fib1 = sum
+    }
+    return sum
+}
 
 /**
  * Простая (2 балла)
  *
  * Для заданного числа n > 1 найти минимальный делитель, превышающий 1
  */
-fun minDivisor(n: Int): Int = TODO()
+fun minDivisor(n: Int): Int {
+    fun simpleNumber(number: Int): Boolean {
+        if (number < 2) return false
+        if (number == 2) return true
+        if (number % 2 == 0) return false
+        for (m in 3..(number/2)) {
+            if (number % m == 0) return false
+        }
+    return true }
+    if (simpleNumber(n) == true) return n
+    for (i in 2..n/2) {
+        if (n % i == 0) return i
+    }
+return 1}
 
 /**
  * Простая (2 балла)
  *
  * Для заданного числа n > 1 найти максимальный делитель, меньший n
  */
-fun maxDivisor(n: Int): Int = TODO()
+fun maxDivisor(n: Int): Int {
+    for (i in n/2 downTo 2) {
+        if (n % i == 0 ) return i
+    }
+return 1}
 
 /**
  * Простая (2 балла)
@@ -120,7 +156,21 @@ fun collatzSteps(x: Int): Int = TODO()
  * Для заданных чисел m и n найти наименьшее общее кратное, то есть,
  * минимальное число k, которое делится и на m и на n без остатка
  */
-fun lcm(m: Int, n: Int): Int = TODO()
+fun lcm(m: Int, n: Int): Int {
+    fun simpleNumber(number: Int): Boolean {
+        if (number < 2) return false
+        if (number == 2) return true
+        if (number % 2 == 0) return false
+        for (m in 3..(number/2)) {
+            if (number % m == 0) return false
+        }
+        return true }
+    if (simpleNumber(n) == true && simpleNumber(m) == true && n == m) return n
+    var a = n*m
+    for (i in 2 .. a) {
+        if (a % i == 0 && i % n ==0 && i % m == 0) return i
+    }
+return 1}
 
 /**
  * Средняя (3 балла)
@@ -129,7 +179,21 @@ fun lcm(m: Int, n: Int): Int = TODO()
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    fun simpleNumber(number: Int): Boolean {
+        if (number < 2) return false
+        if (number == 2) return true
+        if (number % 2 == 0) return false
+        for (m in 3..(number/2)) {
+            if (number % m == 0) return false
+        }
+        return true }
+    if (simpleNumber(m) == true && simpleNumber(n) == true) return true
+    val a = minOf(m,n)
+    for (i in 2.. a) {
+        if (m % i == 0 && n % i == 0) return false
+    }
+return true}
 
 /**
  * Средняя (3 балла)
@@ -138,8 +202,16 @@ fun isCoPrime(m: Int, n: Int): Boolean = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun revert(n: Int): Int = TODO()
-
+fun revert(n: Int): Int {
+    var revNumber = 0
+    var number = n
+    var a = 0
+    while (number > 0) {
+        a = number % 10
+        number/=10
+        revNumber = revNumber*10 + a
+    }
+return revNumber}
 /**
  * Средняя (3 балла)
  *
@@ -149,7 +221,7 @@ fun revert(n: Int): Int = TODO()
  *
  * Использовать операции со строками в этой задаче запрещается.
  */
-fun isPalindrome(n: Int): Boolean = TODO()
+fun isPalindrome(n: Int): Boolean = n == revert(n)
 
 /**
  * Средняя (3 балла)
